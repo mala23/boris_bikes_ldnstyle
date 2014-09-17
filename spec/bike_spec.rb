9c6b7ff673bd27d5e829cfdec1909b859836eed7
mala23 <mail@mala23.me>
2014-09-17 13:48:27 +0100

    Created bike class and bike spec

---
+++ spec/bike_spec.rb
@@ -0,0 +1,14 @@
+# link to the Bike class
+require 'bike'
+
+# we are describing the functionality of a specific class, Bike
+describe Bike do
+	# this is a specific feature (behaviour)
+	# that we expect to be present
+	it "should not be broken after we create it" do
+		the_bike = Bike.new # initialise a new object of Bike class
+		# expect an instance of this class to have
+		# a method "broken?" that should return false
+		expect(the_bike).not_to be_broken
+	end
+end
