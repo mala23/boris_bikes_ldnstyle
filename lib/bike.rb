4f3bab28e326f82330e6f2bf8a7697cb3ff21549
mala23 <mail@mala23.me>
2014-09-17 14:16:48 +0100

    Created methods for bike

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,20 @@
 class Bike
-	def broken?
+
+	def initialize
+		# all instance variables begin with @
+		# this must be an instance variable because we will need
+		# in other methods
+		@broken = false
 	end
+
+	def broken?
+		# instance variables are accessible in all methods
+		@broken
+	end
+
+	def break!
+		# and any instance method can update them
+		@broken = true
+	end
+
 end
