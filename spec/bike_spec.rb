01551b13ed2b3ead2f1da4c049b89d3e33814fa1
mala23 <mail@mala23.me>
2014-09-17 14:29:51 +0100

    DRYed up bike spec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,25 +4,23 @@ require 'bike'
 # we are describing the functionality of a specific class, Bike
 describe Bike do
 
-	# let(:bike) {Bike.new}
+	let(:bike) {Bike.new}
 
 	# this is a specific feature (behaviour)
 	# that we expect to be present
 	it "should not be broken after we create it" do
-		the_bike = Bike.new # initialise a new object of Bike class
 		# expect an instance of this class to have
 		# a method "broken?" that should return false
-		expect(the_bike).not_to be_broken
+		expect(bike).not_to be_broken
 	end
 
 	it "should be able to break" do
-		bike = Bike.new
 		bike.break!
 		expect(bike).to be_broken
 	end
 
 	it "should be able to get fixed" do
-		bike = Bike.new
 		bike.break!
+	end
 
 end
